71736d4d91bb6098757134db0d97941fa9a3ba6b
kanta13jp1 <kanta13jp@gmail.com>
2022-08-01 22:28:39 +0900

    flex sample

--- src/pages/flex.tsx
+++ src/pages/flex.tsx
@@ -5,7 +5,7 @@ import type { NextPage } from 'next';
 const Flex: NextPage = () => {
   return (
     <div className="container mx-auto px-6 py-8">
-      <div className="flex min-h-screen flex-col items-top justify-top py-0 px-2">
+      <div className="flex min-h-screen flex-col items-start justify-items-start py-0 px-2">
         <Head>
           <title>Flex</title>
         </Head>
@@ -14,23 +14,40 @@ const Flex: NextPage = () => {
           <br />
           flex items-center bg-green-200
           <div className="flex items-center bg-green-200">
-            <div className="flex-auto py-4 bg-sky-300">01 flex-auto py-4 bg-sky-300</div>
-            <div className="flex-auto py-12 bg-red-300">02 flex-auto py-12 bg-red-300</div>
-            <div className="flex-auto py-8 bg-yellow-300">03 flex-auto py-8 bg-yellow-300</div>
+            <div className="m-4 flex-auto bg-sky-300 p-4">01 m-4 flex-auto bg-sky-300 p-4</div>
+            <div className="m-12 flex-auto bg-red-300 p-12">02 m-12 flex-auto bg-red-300 p-12</div>
+            <div className="m-8 flex-auto bg-yellow-300 p-8">
+              03 m-8 flex-auto bg-yellow-300 p-8
+            </div>
           </div>
           <br />
           flex items-stretch hover:items-center bg-green-200
-          <div className="flex items-stretch hover:items-center bg-green-200">
-            <div className="flex-auto py-4 bg-sky-300">01 flex-auto py-4 bg-sky-300</div>
-            <div className="flex-auto py-12 bg-red-300">02 flex-auto py-12 bg-red-300</div>
-            <div className="flex-auto py-8 bg-yellow-300">03 flex-auto py-8 bg-yellow-300</div>
+          <div className="flex items-stretch bg-green-200 hover:items-center">
+            <div className="m-4 flex-auto bg-sky-300 p-4">01 m-4 flex-auto bg-sky-300 p-4</div>
+            <div className="m-12 flex-auto bg-red-300 p-12">02 m-12 flex-auto bg-red-300 p-12</div>
+            <div className="m-8 flex-auto bg-yellow-300 p-8">
+              03 m-8 flex-auto bg-yellow-300 p-8
+            </div>
           </div>
           <br />
           flex items-start hover:items-center bg-green-200
-          <div className="flex items-start hover:items-center bg-green-200">
-            <div className="flex-auto py-4 bg-sky-300">01 flex-auto py-4 bg-sky-300</div>
-            <div className="flex-auto py-12 bg-red-300">02 flex-auto py-12 bg-red-300</div>
-            <div className="flex-auto py-8 bg-yellow-300">03 flex-auto py-8 bg-yellow-300</div>
+          <div className="flex items-start bg-green-200 hover:items-center">
+            <div className="m-4 flex-auto bg-sky-300 p-4">01 m-4 flex-auto bg-sky-300 p-4</div>
+            <div className="m-12 flex-auto bg-red-300 p-12">02 m-12 flex-auto bg-red-300 p-12</div>
+            <div className="m-8 flex-auto bg-yellow-300 p-8">
+              03 m-8 flex-auto bg-yellow-300 p-8
+            </div>
+          </div>
+          <br />
+          flex items-baseline hover:items-center bg-green-200
+          <div className="flex items-baseline bg-green-200 hover:items-center">
+            <div className="m-4 bg-sky-300 px-4 pt-2 pb-6">01 m-4 bg-sky-300 px-4 pt-2 pb-6</div>
+            <div className="m-12 bg-red-300 px-12 pt-8 pb-12">
+              02 m-12 bg-red-300 px-12 pt-8 pb-12
+            </div>
+            <div className="m-8 bg-yellow-300 px-8 pt-12 pb-4">
+              03 bg-yellow-300 px-8 pt-12 pb-4
+            </div>
           </div>
         </main>
       </div>
