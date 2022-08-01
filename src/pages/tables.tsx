7efcf2a1eb3167e51463bdf0268dda1f7d780776
kanta13jp1 <kanta13jp@gmail.com>
2022-08-02 01:28:26 +0900

    tables

---
+++ src/pages/tables.tsx
@@ -0,0 +1,108 @@
+import Head from 'next/head';
+
+import type { NextPage } from 'next';
+
+const Tables: NextPage = () => {
+  return (
+    <div className="container mx-auto px-6 py-8">
+      <div className="flex min-h-screen flex-col items-start justify-start py-0 px-2">
+        <Head>
+          <title>Tables</title>
+        </Head>
+        <main>
+          <h1 className="decoration-sky-500">Tables Sample</h1>
+          <br />
+          table-auto border
+          <table className="table-auto border">
+            <thead className="border-b bg-sky-300">
+              <tr>
+                <th className="border-r px-6 py-4 text-left text-sm font-medium text-gray-900">
+                  Song
+                </th>
+                <th className="border-r px-6 py-4 text-left text-sm font-medium text-gray-900">
+                  Artist
+                </th>
+                <th className="border-r px-6 py-4 text-left text-sm font-medium text-gray-900">
+                  Year
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-b bg-white transition duration-300 ease-in-out hover:bg-gray-100">
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">
+                  The Sliding Mr. Bones (Next Stop, Pottersville)
+                </td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">
+                  Malcolm Lockyer
+                </td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">1961</td>
+              </tr>
+              <tr className="border-b bg-white transition duration-300 ease-in-out hover:bg-gray-100">
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">
+                  Witchy Woman
+                </td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">The Eagles</td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">1972</td>
+              </tr>
+              <tr className="border-b bg-white transition duration-300 ease-in-out hover:bg-gray-100">
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">
+                  Shining Star
+                </td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">
+                  Earth, Wind, and Fire
+                </td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">1975</td>
+              </tr>
+            </tbody>
+          </table>
+          <br />
+          table-fixed border
+          <table className="table-fixed border">
+            <thead className="border-b bg-sky-400">
+              <tr>
+                <th className="w-2 border-r px-6 py-4 text-left text-sm font-medium text-gray-900">
+                  Song
+                </th>
+                <th className="border-r px-6 py-4 text-left text-sm font-medium text-gray-900">
+                  Artist
+                </th>
+                <th className="border-r px-6 py-4 text-left text-sm font-medium text-gray-900">
+                  Year
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-b bg-white transition duration-300 ease-in-out hover:bg-gray-100">
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">
+                  The Sliding Mr. Bones (Next Stop, Pottersville)
+                </td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">
+                  Malcolm Lockyer
+                </td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">1961</td>
+              </tr>
+              <tr className="border-b bg-white transition duration-300 ease-in-out hover:bg-gray-100">
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">
+                  Witchy Woman
+                </td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">The Eagles</td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">1972</td>
+              </tr>
+              <tr className="border-b bg-white transition duration-300 ease-in-out hover:bg-gray-100">
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">
+                  Shining Star
+                </td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">
+                  Earth, Wind, and Fire
+                </td>
+                <td className="border-r px-6 py-4 text-sm font-light text-gray-900">1975</td>
+              </tr>
+            </tbody>
+          </table>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Tables;
